952ef8779f1416e01cbc47836e87518e553e962a
Nathan Slamans <slamhouse1122@gmail.com>
2017-10-01 15:15:57 +0000

    created contact table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927015130) do
+ActiveRecord::Schema.define(version: 20171001145556) do
 
-  create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text   "comments"
+  create_table "contact", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
   end
 
 end
